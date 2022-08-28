cd2292c30e86146da1dbe846e444a53f27255658
netomilfont <netomilfont@hotmail.com>
2022-08-28 16:09:53 -0300

    feat: adiciona requisicao editar cliente

--- src/js/api.js
+++ src/js/api.js
@@ -32,8 +32,27 @@ export default class Api {
         return cadUsuario
     }
 
-    static async editarCliente(id, data){
+    static async pequisarCliente (id) {
+        const buscaCliente = await fetch(`${this.BASEURL}/clientes/${id}`, {
+            method: "GET",
+            headers: this.headers
+        })
+        .then(res => res.json())
+        .catch(err => console.log(err))
 
+        return buscaCliente
+    }
+
+    static async editarCliente(id, data){
+        const editCliente = await fetch(`${this.BASEURL}/cliente/${id}`, {
+            method:"PATCH",
+            headers: this.headers,
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .catch(err => console.log(err))
+
+        return editCliente
     }
 
     static async deletarCliente(id){
